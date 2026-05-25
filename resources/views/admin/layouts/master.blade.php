2f382dfe713ebbc3831418e5506a6f7a25faf94a
SteffaniCastro <steffani.castro.useche@gmail.com>
2026-05-25 17:40:33 -0400

    fix favicon

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
-    <link rel="shortcut icon" href="{{ URL::asset('build/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{ URL::asset('build/images/codant.svg')}}">
     @include('admin.layouts.partials.head-css')
     @livewireStyles
 </head>
